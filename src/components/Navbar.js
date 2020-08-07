710f1bc6d77eb75802c754ba787f206688778029
Jaehyun Jun <wjswogus0007@gmail.com>
2020-08-06 20:45:49 -0400

    practice

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import "./Navbar.css"
 
 export const Navbar = () => {
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link id="NavCom" to="/">Home</Link>
         </li>
         <li>
-          <Link to="/career">Career</Link>
+          <Link id="NavCom" to="/career">Career</Link>
         </li>
         <li>
-          <Link to="/investment">Investment</Link>
+          <Link id="NavCom" to="/investment">Investment</Link>
         </li>
       </ul>
     </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar'
-
+import "./index.css"
 export default function Home() {
   return (
     <div>
       <h1>MAIN PAGE</h1>
       <Navbar />
+      <div class="flexCon">
+        <div>
+          <h1>Hello</h1>
+          <p>practice</p>
+        </div>
+        <div>Image</div>
+      </div>
     </div>
   )
 }
