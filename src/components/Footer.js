bbcf2a068d17bac29326fca604e99bff2b93d97f
Jaehyun Jun <wjswogus0007@gmail.com>
2020-08-09 17:49:30 -0400

    Footer Styled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import './Footer.css'
 
 export const Footer = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="www.facebook.com">Image:Facebook</Link>
-        </li>
-        <li>
-          <Link to="www.linkedin.com">Image:LinkedIn</Link>
-        </li>
-        <li>
-          <Link to="www.instagram.com">Image:Instagram</Link>
-        </li>
-      </ul>
+    <div class="footer_background">
+      <div id="footer">
+        <ul>
+          <li>
+            <Link to="www.facebook.com">Image:Facebook</Link>
+          </li>
+          <li>
+            <Link to="www.linkedin.com">Image:LinkedIn</Link>
+          </li>
+          <li>
+            <Link to="www.instagram.com">Image:Instagram</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
