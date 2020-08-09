8e9d0b4e0c5810886f1a1968043b7ea9e03cd73b
Jaehyun Jun <wjswogus0007@gmail.com>
2020-08-09 17:51:59 -0400

    day2

---
+++ src/components/Mail_Form.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Mail_Form.css'
+export const Mail_Form = () => {
+  return (
+    <form action="mailto:yj@yesume.com" method="post" encType="text/plain">
+
+      {/* Name */}
+      <label for="first_name">Name</label>
+      <br></br>
+      <input type="text" id="first_name" name="first_name"
+        placeholder="First Name"></input>
+      <input type="text" id="last_name" name="last_name"
+        placeholder="Last Name"></input>
+      <br></br>
+
+      {/* Email */}
+      <label for="email">E-Mail</label>
+      <br></br>
+      <input type="text" id="email" name="mail"
+        placeholder="example@e-mail.com"></input>
+      <br></br>
+
+      {/* Message */}
+
+      <label for="message">Message</label>
+      <br></br>
+      {/* What is it about? */}
+      <select id="mock">
+        <option value="" disabled selected hidden>
+          What is it about?
+        </option>
+        <option value="mock1">mock1</option>
+        <option value="mock2">mock2</option>
+        <option value="mock3">mock3</option>
+        <option value="mock4">mock4</option>
+      </select>
+      <br></br>
+      <textarea id="message" placeholder="inquires"></textarea>
+
+      {/* Submit */}
+      <input type="submit" value="Submit"></input>
+    </form >
+  )
+}
