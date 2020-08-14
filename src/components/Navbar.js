89196f2071c4ee505bf1d4781dc772eb9974a15c
Jaehyun Jun <wjswogus0007@gmail.com>
2020-08-14 16:29:06 -0400

    basic styled added to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,48 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import './Navbar_style.css'
 
 export const Navbar = () => {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/career">Career</Link>
-        </li>
-        <li>
-          <Link to="/investment">Investment</Link>
-        </li>
-      </ul>
+
+      <div class="textNav">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/career">Career</Link>
+          </li>
+          <li>
+            <Link to="/investment">Investment</Link>
+          </li>
+        </ul>
+      </div>
+
+      <div class="socialNav">
+        <ul>
+          <li>
+            <Link to="https://www.facebook.com">
+              <img src={'../../images/icon_facebook.png'}
+                alt="facebook icon"></img>
+            </Link>
+          </li>
+          <li>
+            <Link to="https://www.instagram.com">
+              <img src={'../../images/icon_instagram.png'}
+                alt="instagram icon"></img>
+            </Link>
+          </li>
+          <li>
+            <Link to="https://www.linkedin.com">
+              <img src={'../../images/icon_linkedin.png'}
+                alt="linkedin icon"></img>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
     </nav>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import { Navbar } from '../components/Navbar'
 export default function Home() {
   return (
     <div>
-      <h1>MAIN PAGE</h1>
       <Navbar />
+      <h1>MAIN PAGE</h1>
     </div>
   )
 }
