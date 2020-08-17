85e23de4dfa9134397d3ec2d7b8791812e12b5ad
Jaehyun Jun <wjswogus0007@gmail.com>
2020-08-16 20:31:22 -0400

    added footer to index.js, minor code error from Navbar_style.css is corrected

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar'
+import { Footer } from '../components/Footer'
 import './index.css'
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <Navbar />
       <h1>MAIN PAGE</h1>
+      <Footer />
     </div>
   )
 }
