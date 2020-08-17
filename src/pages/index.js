32e689747310339e2859063e2abe38a435ce1d61
Jaehyun Jun <wjswogus0007@gmail.com>
2020-08-16 22:13:38 -0400

    footer removed from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar'
-import { Footer } from '../components/Footer'
 import './index.css'
 
 export default function Home() {
@@ -8,7 +7,6 @@ export default function Home() {
     <div>
       <Navbar />
       <h1>MAIN PAGE</h1>
-      <Footer />
     </div>
   )
 }
