256569e41826f11c014922d06f7fd96ddcd0c75e
we11done <wjswogus0007@gmail.com>
2020-08-30 23:42:52 -0400

    Navbar relative padding changed to fixed padding, Footer imported to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar'
+import { Footer } from '../components/Footer'
 import './index.css'
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   )
 }
