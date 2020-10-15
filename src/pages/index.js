49fdd06e8fbe9c0c5a0cfb29a04a5541a6b1469c
Jaehyun Jun <wjswogus0007@gmail.com>
2020-10-15 17:03:49 -0400

    Permission changed on Footer files using chmod -R 777, Background color of Navbar and Footer edited, Added LandingPage component to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Navbar } from '../components/Navbar'
 import { Footer } from '../components/Footer'
 import './index.css'
+import { LandingPage } from '../components/LandingPage'
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <LandingPage />
       <Footer />
     </div>
   )
